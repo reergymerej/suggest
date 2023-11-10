78560dd9488b9dbf2e7d3b8e032cb506a2ddad5a
Jeremy Greer <jex.grizzle@gmail.com>
2023-11-10 07:35:54 -0500

    Happy path complete

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 
+const assert = require('assert')
+
 const words = [
-    'lady',
-    'ladybird',
-    'land',
-    'laser',
-    'laserbeam',
-    'laserface',
-    'lasergun',
-    'lemon',
-    'lemons',
-    'less',
-    'lesson',
-    'lord',
-    'lordhuron',
+  'lady',
+  'ladybird',
+  'land',
+  'laser',
+  'laserbeam',
+  'laserface',
+  'lasergun',
+  'lemon',
+  'lemons',
+  'less',
+  'lesson',
+  'lord',
+  'lordhuron',
 ]
 
 let tree = {}
@@ -31,4 +33,34 @@ words.map(word => {
 })
 
 
-console.log(JSON.stringify(tree, null, 2))
+const print = (obj) => console.log(JSON.stringify(obj, null, 2))
+
+const expandNode = (prefix, node) => {
+  let words = []
+  for (const [k, v] of Object.entries(node)) {
+    if (typeof v === 'object') {
+      words = [...words, ...expandNode(prefix + k, v)]
+    } else {
+      words = [...words, prefix]
+    }
+  }
+  return words
+}
+
+const suggest = (tree, prefix) => {
+  let node = tree
+  prefix.split('').map(letter => {
+    node = node[letter]
+  })
+  // TODO: bail when we have no suggestions
+  return expandNode(prefix, node)
+}
+
+const actual = suggest(tree, 'las')
+const expected = [
+  'laser',
+  'laserbeam',
+  'laserface',
+  'lasergun',
+]
+assert.deepEqual(actual, expected)
